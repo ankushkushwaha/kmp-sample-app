981c28adf31d35493a5be5d3f18a8e6320e12fd9
ankushkushwaha <ankush7377@gmail.com>
2025-06-22 21:09:47 +0530

    Add unit test for user view model

--- shared/src/commonMain/kotlin/data/Network/NetworkClient.kt
+++ shared/src/commonMain/kotlin/data/Network/NetworkClient.kt
@@ -6,9 +6,9 @@ import io.ktor.client.request.*
 
 import io.ktor.client.call.body
 
-class UserRepository(private val client: HttpClient = createHttpClient()) {
+open class UserRepository(private val client: HttpClient = createHttpClient()) {
 
-    suspend fun fetchUsers(): List<User> {
+    open suspend fun fetchUsers(): List<User> {
         // Perform the network request and parse the JSON response
         return try {
             client.get("https://jsonplaceholder.typicode.com/users").body()
