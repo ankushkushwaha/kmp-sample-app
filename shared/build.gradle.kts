e2ad49dee9bb5c5c9179fef4494e14b78ebfb4b9
ankushkushwaha <ankush7377@gmail.com>
2025-07-01 10:05:00 +0530

    Rename shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -24,7 +24,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
-            baseName = "shared"
+            baseName = "Shared"
             isStatic = true
         }
     }
