b4bd7a8b3c446222558abde3a395fe1f33056d71
ankushkushwaha <ankush7377@gmail.com>
2025-07-02 11:06:00 +0530

    Improve navigation

--- androidApp/src/main/java/com/example/kmp_sample_app/android/MainContentScreen.kt
+++ androidApp/src/main/java/com/example/kmp_sample_app/android/MainContentScreen.kt
@@ -1,9 +1,5 @@
 import Presentation.ContentViewModel
-import Presentation.UserViewModel
 import android.content.Intent
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -11,15 +7,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kmp_sample_app.android.TodosActivity
-import com.example.kmp_sample_app.android.TodosScreen
 import com.example.kmp_sample_app.android.UserListActivity
-import com.example.kmp_sample_app.android.UserListScreen
 
 @Composable
 fun MainContentScreen(
@@ -44,6 +38,8 @@ fun MainContentScreen(
                             Intent(context, TodosActivity::class.java)
                         )
                     }
+
+                    ContentViewModel.ContentOptions.NONE -> TODO()
                 }
             }
         )
