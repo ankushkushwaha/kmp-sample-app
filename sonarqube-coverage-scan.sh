d42f562fef189e9f48e4929e649376f007126936
ankushkushwaha <ankush7377@gmail.com>
2025-08-07 17:49:59 +0530

    Run all coverage by default in run script

--- sonarqube-coverage-scan.sh
+++ sonarqube-coverage-scan.sh
@@ -8,11 +8,11 @@ echo "🔁 Starting clean and full coverage pipeline..."
 chmod +x ./gradlew
 
 #echo "🚀 Running unified coverage scan (androidApp + iosApp + shared)..."
-#./gradlew allKmpCoverage --no-configuration-cache
+./gradlew allKmpCoverage --no-configuration-cache
 
 
 #echo "🚀 Running iOS + shared coverage scan (iosApp + shared)..."
-./gradlew iosCoverage --no-configuration-cache
+#./gradlew iosCoverage --no-configuration-cache
 
 #echo "🚀 Running android + shared coverage scan (androidApp + shared)..."
 #./gradlew androidCoverage --no-configuration-cache
