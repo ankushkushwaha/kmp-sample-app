3bb8c3de7dcb7427aa4fc3d0ef431c761577cf56
ankushkushwaha <ankush7377@gmail.com>
2025-08-07 17:48:11 +0530

    Add sonar script to automate coverage report scan

---
+++ sonarqube-coverage-scan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "🔁 Starting clean and full coverage pipeline..."
+
+# Make sure Gradle is executable
+chmod +x ./gradlew
+
+#echo "🚀 Running unified coverage scan (androidApp + iosApp + shared)..."
+#./gradlew allKmpCoverage --no-configuration-cache
+
+
+#echo "🚀 Running iOS + shared coverage scan (iosApp + shared)..."
+./gradlew iosCoverage --no-configuration-cache
+
+#echo "🚀 Running android + shared coverage scan (androidApp + shared)..."
+#./gradlew androidCoverage --no-configuration-cache
+
+
+echo "✅ All done! Reports generated and sent to SonarQube."
